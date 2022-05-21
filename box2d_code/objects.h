85333ca12ac7a9a4586cee5fef5e8fefd01c9996
Marcin Kowalczyk <01169191@pw.edu.pl>
2022-05-21 22:29:04 +0200

    MouseTracker implmented

--- box2d_code/objects.h
+++ box2d_code/objects.h
@@ -8,35 +8,22 @@ public:
     DynamicObject(b2World&, float, float);
     b2Vec2 getPosition() const;
     float getAngle() const;
-    const void setNewPosition(const b2Vec2&, float);
-    void putVelocity(b2Vec2);
-    void putForceToCenter(b2Vec2);
-    b2Body* body;
-
-protected:
-    b2BodyDef def;
-};
-
-
-
-
-////////////////////////////////////////////////////
-//Polygon
-////////////////////////////////////////////////////
-
-
-//Polygon dziedziczy po dynamic object
-class Polygon : public DynamicObject
-{
-public:
-    Polygon(b2World& world, float new_x, float new_y, const b2Vec2* setVertices, int n_vertices);
     b2Fixture* getFixture(int idx) const;
     const b2Shape* getShape(int idx);
-    const b2Vec2* getVertices();
 
+    void setNewPosition(const b2Vec2&, float);
+    void putVelocity(const b2Vec2);
+    void putForceToCenter(const b2Vec2);
+ 
 protected:
+    b2BodyDef def;
+    b2Body* body;
+    
+    void addFixture(const b2FixtureDef*);
 
+    friend class Spring;
 };
+
 
 
 
@@ -44,12 +31,48 @@ protected:
 //Worm
 ////////////////////////////////////////////////////
 
-class Worm : public Polygon
+
+//Worm dziedziczy po dynamic object
+class Worm  : public DynamicObject
 {
 public:
-    //Worm(b2World& world, float new_x, float new_y, const b2Vec2* setVertices, int n_vertices);
-    //void jump();
-    //
-private:
+    Worm(b2World& world, float new_x, float new_y);
+    //const b2Vec2* getVertices();
+    
+    void addWeapon();
+    
+    void jump();
+    
 
+
+protected:
+    b2PolygonShape shapePolygon;
+    b2FixtureDef fixtureDef;
+
+    bool isOnGround();
+};
+
+
+class Weapon : public DynamicObject
+{
+
+};
+
+
+
+class StaticObject
+{
+
+};
+
+
+class Spring
+{
+public:
+    Spring();
+    void bind(b2World*, const DynamicObject*);
+    void update(b2Vec2&);
+
+protected:
+    b2MouseJoint* mj;
 };
