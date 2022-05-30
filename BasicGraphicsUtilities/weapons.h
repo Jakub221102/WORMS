4047e465bd7fe4d62fea654e329dcf3bf87bea2c
Piotras laptop <Piotr.kow.59@gmail.com>
2022-05-30 19:10:43 +0200

    grenade(dynamicObject) created

---
+++ BasicGraphicsUtilities/weapons.h
@@ -0,0 +1,23 @@
+#include <dynamic_game_object.h>
+#include <vector>
+#include <box2d.h>
+# include <stdexcept>
+
+class Grenade : public GR::DynamicObject
+{
+    private:
+        float currentTime = 0;
+        float explosionTime = 3;
+    public:
+        void SetExplosionTime(float newTime) { explosionTime = newTime; }
+        float GetExplosionTime() const { return explosionTime; }
+
+        void IncCurrentTime(float deltaTime) { currentTime += deltaTime; }
+        float GetCurrentTime() const { return currentTime; }
+
+        void GrenadeExplosion();
+        void update();
+        void putStartingVelocity(sf::Vector2i mousePosition, float MouseTimeHold);
+        Grenade(b2World& world, const float& time, std::vector<std::pair<float, float>> vertices, std::string texture_path,
+            sf::Vector2f& startingPos, sf::Vector2f& mousePosition, float MouseTimeHold);
+};
