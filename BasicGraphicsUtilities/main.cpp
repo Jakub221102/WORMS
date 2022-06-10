b14c8d5e6614074efad773668b1260d56472be19
jwozniak <01169260@pw.edu.pl>
2022-06-10 03:50:09 +0200

    ADAM MAŁYSZ PROSZE PAŃSTWA CO ZA SKOK

--- BasicGraphicsUtilities/main.cpp
+++ BasicGraphicsUtilities/main.cpp
@@ -630,13 +630,13 @@ int main() {
 	icon.setRelativeVector({ 73.0f, 30.0f });
 
 	Worm::addKeyBinding(sf::Keyboard::W, &Worm::jump, InputType::REALTIME);
-	Worm::setKeyArguments(sf::Keyboard::W, { 0.0f, 25.0f }, InputType::REALTIME); // pass velocity to jump method
+	Worm::setKeyArguments(sf::Keyboard::W, { 0.0f, 35.0f }, InputType::REALTIME); // pass velocity to jump method
 	Worm::addKeyBinding(sf::Keyboard::D, &Worm::move_right, InputType::REALTIME);
 	Worm::setKeyArguments(sf::Keyboard::D, { 0.5f, 0.0f }, InputType::REALTIME); // pass velocity to move method
 	Worm::addKeyBinding(sf::Keyboard::A, &Worm::move_left, InputType::REALTIME);
 	Worm::setKeyArguments(sf::Keyboard::A, { -0.5f, 0.0f }, InputType::REALTIME); // pass velocity to move method
 	Worm::addKeyBinding(sf::Keyboard::S, &Worm::move_down, InputType::REALTIME);
-	Worm::setKeyArguments(sf::Keyboard::S, { 0.0f, -25.0f }, InputType::REALTIME); // pass velocity to move method
+	Worm::setKeyArguments(sf::Keyboard::S, { 0.0f, -15.0f }, InputType::REALTIME); // pass velocity to move method
 	//obj.addKeyBinding(sf::Keyboard::Q, &Worm::shot, InputType::REALTIME);
 	//obj.setKeyArguments(sf::Keyboard::Q, {0 , 0}, InputType::REALTIME);
 	Worm::addKeyBinding(sf::Keyboard::Num1, &Worm::pickWeapon1, InputType::REALTIME);
